83f64bfb59a07d94f78d0b07201fe766d3eb8da4
Yureez1 <j.banchongphanith@gmail.com>
2025-08-27 09:46:26 +0200

    Last

---
+++ CPP02/ex00/Fixed.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbanchon <jbanchon@student42.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/26 12:25:24 by jbanchon          #+#    #+#             */
+/*   Updated: 2025/08/26 21:16:28 by jbanchon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+
+private:
+    int _value;
+    static const int _fractionalBits = 8;
+
+public:
+    Fixed();
+    Fixed(const Fixed& other);
+    Fixed& operator=(const Fixed& other);
+    ~Fixed();
+
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+};
+
+#endif
